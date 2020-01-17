b1f293b03e500a05cd2c0149668f9fb60e7e9017
Michael <skaman4459@gmail.com>
2020-01-17 11:13:26 -0500

    added files

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
+const projectsRouter = require('./data/helpers/projectsRouter');
+const actionsRouter = require('./data/helpers/actionsRouter');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
 
 module.exports = server;
