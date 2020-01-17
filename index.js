00f809eabd32ca02cea990e44696688ccb86e363
Michael <skaman4459@gmail.com>
2020-01-17 11:09:37 -0500

    server setup

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const server = require('./server');
+
+const port = process.env.PORT || 8000; // dynamic yo
+
+server.listen(port, () => {
+    console.log(`\n* Server is running on http://localhost on port ${port}*\n`)
+})
